b9d378bf05e18b7c6d6bff75b88e5ae482ee6610
joseojeda <joseojeda@seti.com.co>
2025-04-29 12:39:39 -0500

    Implementa endpoint en openAi config

--- agents/base_agent.py
+++ agents/base_agent.py
@@ -12,18 +12,21 @@ class BaseAgent:
     def _setup_kernel(self):
         # Configurar el Semantic Kernel con OpenAI
         api_key = OpenAIConfig.API_KEY
+        
         self.kernel.add_chat_service(
             service_id=self.name,
             service=OpenAIChatCompletion(
                 model_id=self.model_name,
-                api_key=api_key
+                api_key=api_key,
+                endpoint=OpenAIConfig.ENDPOINT,
             )
         )
         self.kernel.add_text_embedding_generation_service(
             service_id="embedding",
             service=OpenAITextEmbedding(
                 model_id=OpenAIConfig.EMBEDDING_MODEL,
-                api_key=api_key
+                api_key=api_key,
+                endpoint=OpenAIConfig.ENDPOINT,
             )
         )
     
@@ -36,12 +39,12 @@ class BaseAgent:
             skills.update(semantic_skills)
 
         if native:
-            print(native)
+            # print(native)
             for name, skill_instance in native.items():
                 imported_skill = self.kernel.import_skill(skill_instance, name)
                 skills.update(imported_skill)
 
-        print(skills)
+        # print(skills)
 
         return skills
         
