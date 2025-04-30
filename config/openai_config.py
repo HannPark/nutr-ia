2d86caee437643e40fe7ad5f91bfb529f7f52c23
joseojeda <joseojeda@seti.com.co>
2025-04-29 19:04:38 -0500

    Cambia endpoint y modelos

--- config/openai_config.py
+++ config/openai_config.py
@@ -6,10 +6,10 @@ load_dotenv()
 
 class OpenAIConfig:
     API_KEY = os.getenv("GITHUB_TOKEN")
-    ENDPOINT = "https://models.inference.ai.azure.com"
-    CONSULTANT_MODEL = "gpt-4o"
-    NUTRITIONIST_MODEL = "gpt-4o"
-    SEARCHER_MODEL = "gpt-4o-mini"
+    ENDPOINT = "https://models.github.ai/inference"
+    CONSULTANT_MODEL = "meta/Llama-4-Maverick-17B-128E-Instruct-FP8"
+    NUTRITIONIST_MODEL = "openai/gpt-4.1"
+    SEARCHER_MODEL = "openai/gpt-4.1"
     EMBEDDING_MODEL = "text-embedding-3-small"
     MAX_TOKENS = 4000
     TEMPERATURE = 0.1
